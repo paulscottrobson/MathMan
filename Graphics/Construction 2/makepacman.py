3ddb981ae6c1a4ee02341210c948b8c306921980
Paul Robson <paulscottrobson@gmail.com>
2015-08-19 18:14:06 +0100

    Fixed Pacman 3D Rotate

---
+++ Graphics/Construction 2/makepacman.py
@@ -0,0 +1,15 @@
+#
+#	Chop segments out of Pacman.
+#
+from PIL import Image,ImageDraw
+
+img = Image.open("pacman.png")
+for angle in [0,90,180,270]:
+	base = "pacman_"+str(angle)
+	img.save(base+"_0.png")
+	for gap in [1,2]:
+		opimg = img.copy()
+		gapAngle = gap * 20
+		draw = ImageDraw.Draw(opimg)
+		draw.pieslice([(0,0),opimg.size],angle-gapAngle,angle+gapAngle,fill = (0,0,0,0))
+		opimg.save(base+"_"+str(gap)+".png")
