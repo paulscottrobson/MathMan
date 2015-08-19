0598edf7bd804e1d3be2273b6b7f3a73e3718368
Paul Robson <paulscottrobson@gmail.com>
2015-08-19 22:39:26 +0100

    Smoothed Pacman Graphic

--- Graphics/Construction 2/makepacman.py
+++ Graphics/Construction 2/makepacman.py
@@ -7,9 +7,9 @@ img = Image.open("pacman.png")
 for angle in [0,90,180,270]:
 	base = "pacman_"+str(angle)
 	img.save(base+"_0.png")
-	for gap in [1,2]:
+	for gap in range(1,6):
 		opimg = img.copy()
-		gapAngle = gap * 20
+		gapAngle = gap * 8
 		draw = ImageDraw.Draw(opimg)
 		draw.pieslice([(0,0),opimg.size],angle-gapAngle,angle+gapAngle,fill = (0,0,0,0))
 		opimg.save(base+"_"+str(gap)+".png")
